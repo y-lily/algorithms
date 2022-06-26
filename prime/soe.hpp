edd359597b1810c5f401512dc47726d7b7aba549
y-lily <yisitlily@gmail.com>
2022-06-27 00:13:29 +0300

    Replace the SoE function entirely, add comments
    Instead of putting found primes to a given stream, the function now modifies a bitset of custom length passed to it. The principles stay almost the same, but the burden to use the result numbers is now on the client.
    This allows to only check for primality numbers within [2; sqrt(N)] range, which fastens the function significantly.
    
    The function is now a template and only takes a bitset of a custom uint64_t size.
    
    Also, better descriptions of the algorithm and the function were provided.

--- prime/soe.hpp
+++ prime/soe.hpp
@@ -1,41 +1,58 @@
-// Output prime numbers to an ostream using the Sieve of Eratosthenes.
-// The algorithm's description:
-//   1. Create a list of N elements with all elements initially enabled.
-//   2. Start with P = 2 (the smallest prime number).
-//   3. If P is enabled, write it down (as it is a prime number) and disable every Pth element.
-//   4. If P is not greater than N, increment it by 1 and return to the step 3.
+/**
+ * @file soe.hpp
+ * @brief Implementation of the Sieve of Eratosthenes (SoE) algorithm.
+ * @details (https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes)
+ * SoE is an algorithm for finding prime numbers up to a specific limit.
+ * The algorithm:
+ * - in a bit array of length n, set every bit to 1, except for 0 and 1;
+ * - starting from p = 2, for each p set to 1, set to 0 all bits which are
+ * multiples of p, except for p itself.
+ * The result is an array of length n, where an index i is set to 1 if and
+ * only if an integer number i is prime.
+ *
+ * ### Time complexity
+ * O(n log log n)
+ *
+ * ### Space complexity
+ * O(n)
+ *
+ * @author y-lily
+ * @version 0.3
+ * @date 2022-06-26
+ */
+
 #include <bitset>
-#include <ostream>
-#include <string>
+#include <cmath>
+
+using std::bitset;
 
 
-using ul = unsigned long;
-using std::bitset, std::ostream, std::string;
-
-const auto mask_size = 60'000'000;
-
-
-inline void set_each(ul start, ul end, ul step, bitset<mask_size>& mask, bool value = true) {
-  for (auto i = start; i <= end && i >= step; i += step) {
-    mask.set(i, value);
-  }
+template <uint64_t N>
+inline void set_each(bitset<N>& numbers, uint64_t start, uint64_t step, bool value) {
+    for (auto i = start; i < N; i += step) {
+        numbers.set(i, value);
+    }
 }
 
-/**
- * @brief Outputs a sequence of the prime numbers within a specific range.
- *
- * @param os The output stream which recieves the result numbers.
- * @param limit An upper bound of the search range; should be less than mask_size.
- * @param delim A delimiter sent to the ostream after every found prime.
- */
-void generate_primes(ostream& os, ul limit, const string& delim = "\n") {
-  bitset<mask_size> mask;
-  set_each(2, limit, 1, mask);
 
-  for (ul i = 2; i <= limit && i >= 2; ++i) {
-    if (mask[i] == true) {
-      os << i << delim;
-      set_each(i, limit, i, mask, false);
+/**
+ * @brief Within a bitset with indices representing integer numbers, sets the
+ * indices corresponding to prime numbers true and the indices corresponding
+ * to non-prime numbers false.
+ *
+ * @tparam N The size of the bitset and the (excluded) upper limit of the
+ * search range.
+ * @param numbers The bitset with indices representing integers [0; N - 1].
+ */
+template <uint64_t N>
+void set_primes(bitset<N>& numbers) {
+    numbers.set(0, false);
+    numbers.set(1, false);
+    set_each(numbers, 2, 1, true);
+
+    for (uint64_t p = 2; p <= std::sqrt(N); ++p) {
+        if (numbers[p]) {
+            set_each(numbers, p + p, p, false);
+        }
     }
-  }
 }
