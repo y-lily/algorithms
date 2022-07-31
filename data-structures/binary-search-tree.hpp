cf5ee513f6f25b8be1e98f2e9367fe0ccba431b0
y-lily <yisitlily@gmail.com>
2022-07-31 21:35:26 +0300

    Add more inner methods
    All the inner logic from the book on algorithms - search, min, max,
    predecessor, successor, insert, delete - is either written or designated
     as prototypes.

--- data-structures/binary-search-tree.hpp
+++ data-structures/binary-search-tree.hpp
@@ -1,5 +1,4 @@
 #include <functional>
-#include <iostream>
 #include <memory>
 
 
@@ -18,7 +17,7 @@ private:
     std::shared_ptr<Node> root = nullptr;
 
 
-    void inorder_walk(std::shared_ptr<Node>& x, std::function<void(T)> action) {
+    void inorder_walk(std::shared_ptr<Node>& x, const std::function<void(T)>& action) {
         if (!x) { return; }
 
         inorder_walk(x->left, action);
@@ -26,20 +25,94 @@ private:
         inorder_walk(x->right, action);
     }
 
-    std::shared_ptr<Node> search(const std::shared_ptr<Node>& x, const T& k) {
-        if (!x || k == x->key) { return x; }
+    std::shared_ptr<Node>& search(const std::shared_ptr<Node>& x, const T& k) {
+        if ((!x) || (k == x->key)) { return x; }
 
-        return k < x->key ? search(x->left, k) : search(x->right, k);
+        return (k < x->key) ? (search(x->left, k)) : (search(x->right, k));
     }
 
-    std::shared_ptr<Node> iterative_search(std::shared_ptr<Node> x, const T& k) {
+    std::shared_ptr<Node>& iterative_search(std::shared_ptr<Node> x, const T& k) {
         while ((x) && (k != x->key)) {
-            x = k < x->key ? x->left : x->right;
+            x = (k < x->key) ? (x->left) : (x->right);
         }
 
         return x;
     }
 
+    std::shared_ptr<Node>& minimum(std::shared_ptr<Node> x) {
+        while (x->left) {
+            x = x->left;
+        }
+
+        return x;
+    }
+
+    std::shared_ptr<Node>& maximum(std::shared_ptr<Node> x) {
+        while (x->right) {
+            x = x->right;
+        }
+
+        return x;
+    }
+
+    std::shared_ptr<Node>& predecessor(std::shared_ptr<Node> x) {
+        if (x->left) { return maximum(x->left); }
+
+        auto p = x->parent;
+        while ((p) && (x == p->left)) {
+            x = p;
+            p = x->parent;
+        }
+
+        return p;
+    }
+
+    std::shared_ptr<Node>& successor(std::shared_ptr<Node> x) {
+        if (x->right) { return minimum(x->right); }
+
+        auto s = x->parent;
+        while ((s) && (x == s->right)) {
+            x = s;
+            s = x->parent;
+        }
+
+        return s;
+    }
+
+    void insert(const T& v) {
+        if (!root) {
+            root = std::make_shared<Node>(Node { v });
+            return;
+        }
+
+        auto x = root;
+        auto p = x;
+
+        while (x) {
+            p = x;
+            x = (v < x->key) ? (x->left) : (x->right);
+        }
+
+        x = std::make_shared<Node>(Node { v });
+        x->parent = p;
+
+        if ((x->key) < (p->key)) {
+            p->left = x;
+        }
+        else {
+            p->right = x;
+        }
+    }
+
+    // void transplant(std::shared_prt<Node>& x, std::shared_ptr<Node>& y);
+    // void remove(std::shared_ptr<Node>& x);
+
 public:
 
+    // void push(const T& value);
+    // T& pop(const T& value);
+    // bool contains(const T& value) const;
+    // T min() const;
+    // T max() const;
+    // T root_value() const;
 };
